d6d8ad4a215e61b5e87da52f9d5fd8530564451b
davidbtosh <david@mtosh.net>
2016-11-29 11:35:01 +1100

    Fixed web essentials issues

--- AvantPoc/AvantPoc/Controllers/HomeController.cs
+++ AvantPoc/AvantPoc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AvantPoc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,10 @@ namespace AvantPoc.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            PolicyDetailsInfo model = new PolicyDetailsInfo();
+            model.CreateMockData();
+
+            return View(model);
         }
     }
 }
